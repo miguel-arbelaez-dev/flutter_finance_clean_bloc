d5def6cd9f3dda2a520b4667c45609bed50731c3
Miguel Arbelaez <miguelarbelaezdev@gmail.com>
2026-01-30 18:54:31 -0500

    add finance dashboard, form and UI widgets 2

--- flutter_finance_clean_bloc/lib/app/app.dart
+++ flutter_finance_clean_bloc/lib/app/app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_finance_clean_bloc/features/finance/presentation/pages/dashboard_page.dart';
-import '../features/finance/presentation/bloc/finance_bloc.dart';
 import 'app_providers.dart';
 
 class App extends StatelessWidget {
